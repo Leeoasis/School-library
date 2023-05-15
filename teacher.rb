5bf9f427059b069ab2f122706b6bdf8e31baeaa2
leslie <Kooliesk@gmail.com>
2023-05-15 16:55:14 +0200

    add teacher class

---
+++ teacher.rb
@@ -0,0 +1,17 @@
+require_relative 'person'
+
+class Teacher < Person
+    attr_accessor :specialization
+
+    def initialize(age, specialization, name = "Unknown", parent_permission = true)
+        super(age, name, parent_permission)
+        @specialization = specialization
+    end
+
+    def can_use_services?
+        true
+    end
+end
+
+teacher = Teacher.new(34, "Math", "James" )
+puts teacher.name
