458614ba888679756f2347551b40a0101de7a506
leslie <Kooliesk@gmail.com>
2023-05-15 17:21:43 +0200

    Add REAMDE file

--- teacher.rb
+++ teacher.rb
@@ -1,17 +1,14 @@
 require_relative 'person'
 
 class Teacher < Person
-    attr_accessor :specialization
+  attr_accessor :specialization
 
-    def initialize(age, specialization, name = "Unknown", parent_permission = true)
-        super(age, name, parent_permission)
-        @specialization = specialization
-    end
+  def initialize(age, specialization, name: 'Unknown', parent_permission: true)
+    super(age, name, parent_permission)
+    @specialization = specialization
+  end
 
-    def can_use_services?
-        true
-    end
+  def can_use_services?
+    true
+  end
 end
-
-teacher = Teacher.new(34, "Math", "James" )
-puts teacher.name
