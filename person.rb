c4469a9a50b70741d28d190efb25f3f9d2d04b65
leslie <Kooliesk@gmail.com>
2023-05-16 15:46:14 +0200

    Implement composition

--- person.rb
+++ person.rb
@@ -1,12 +1,23 @@
-class Person
+class Nameable
+  def correct_name?
+      raise NotImplementedError, "Method 'correct_name' must be implemented in a subclass."
+  end
+end
+
+class Person < Nameable
   attr_reader :id
   attr_accessor :name, :age
 
   def initialize(age, name: 'Unknown', parent_permission: true)
+    super() # This will call the initialize method in the parent class (Nameable)
     @id = rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
+  end
+
+  def correct_name
+    @name
   end
 
   def can_use_services?
@@ -19,3 +30,31 @@ class Person
     @age >= 18
   end
 end
+
+class BaseDecorator < Nameable
+  def initialize(nameable)
+    @nameable = nameable
+  end
+
+  def correct_name
+    @nameable.correct_name
+  end
+end
+
+class CapitalizeDecorator < BaseDecorator
+  def correct_name
+    @nameable.correct_name.capitalize
+  end
+end
+
+class TrimmerDecorator < BaseDecorator
+  def correct_name
+    trimmed_name = @nameable.correct_name[0, 10]
+    trimmed_name.length < @nameable.correct_name.length ? "#{trimmed_name}..." : trimmed_name
+  end
+end
+
+
+
+
+
