68edbfb5b6ac21d3e492eeadeb5dd1c3417cd777
leslie <Kooliesk@gmail.com>
2023-05-16 16:28:31 +0200

    add nameable class to a seperate file

--- person.rb
+++ person.rb
@@ -1,8 +1,6 @@
-class Nameable
-  def correct_name?
-    raise NotImplementedError, "Method 'correct_name' must be implemented in a subclass."
-  end
-end
+require_relative 'nameable'
+require_relative 'trimmer_decorator'
+require_relative 'capitalize_decorator'
 
 class Person < Nameable
   attr_reader :id
@@ -31,26 +29,7 @@ class Person < Nameable
   end
 end
 
-class BaseDecorator < Nameable
-  def initialize(nameable)
-    super()
-    @nameable = nameable
-  end
-
-  def correct_name
-    @nameable.correct_name
-  end
-end
-
-class CapitalizeDecorator < BaseDecorator
-  def correct_name
-    @nameable.correct_name.capitalize
-  end
-end
-
-class TrimmerDecorator < BaseDecorator
-  def correct_name
-    trimmed_name = @nameable.correct_name[0, 10]
-    trimmed_name.length < @nameable.correct_name.length ? "#{trimmed_name}..." : trimmed_name
-  end
-end
+person = Person.new(17, name: 'leslie')
+puts person.correct_name
+capitalizedPerson = CapitalizeDecorator.new(person)
+puts capitalizedPerson.correct_name
