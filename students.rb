83402bc4524d3b24705408c705c134beffb4dea1
leslie <Kooliesk@gmail.com>
2023-05-18 13:13:33 +0200

    add classroom method

--- students.rb
+++ students.rb
@@ -5,10 +5,15 @@ class Student < Person
 
   def initialize(classroom, age, name: 'Unknown', parent_permission: true)
     super(age, name: name, parent_permission: parent_permission)
-    @classroom = classroom
+    self.classroom = classroom
   end
 
   def play_hooky
     '¯\\(ツ)/¯'
+  end
+
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.students.push(self) unless classroom.students.include?(self)
   end
 end
